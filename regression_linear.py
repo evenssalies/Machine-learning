26181ab8b9db5c65cc7f15705b0e45695c9ace8c
evenssalies <32770815+evenssalies@users.noreply.github.com>
2023-10-24 10:35:37 +0200

    regression_linear, k-neighbors

--- regression_linear.py
+++ regression_linear.py
@@ -1,3 +1,6 @@
+# Géron (2022, pp.25-26)
+#   Ch. 1, section "Instance-based versus model-based learning"
+
 import  matplotlib.pyplot as plt
 import  numpy as np
 import  pandas as pd
@@ -20,6 +23,18 @@ plt.show()
 model = LinearRegression()
 
 # Train the model
+model.fit(X, y)
+
+# Make a prediction for Cyprus
+#   Cyprus' GDP per capita in 2020
+X_new = [[37_655.2]]
+print(model.predict(X_new))
+
+# Instance-based learning (k-nearest neighbors)
+from    sklearn.neighbors import KNeighborsRegressor
+model = KNeighborsRegressor(n_neighbors=3)
+
+# Train the new model
 model.fit(X, y)
 
 # Make a prediction for Cyprus
