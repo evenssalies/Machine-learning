3e4263646d40675c238715c7ad91f40b8885f235
evenssalies <32770815+evenssalies@users.noreply.github.com>
2024-06-20 16:01:07 +0200

    Split the code into dataset import and dataset training

---
+++ regression_map_data.py
@@ -0,0 +1,31 @@
+# Géron (2022)
+#   Ch. 2, section "Instance-based versus model-based learning"
+#   Import the dataset
+
+from    pathlib import Path
+import  pandas as pd
+import  tarfile         # Library to dezip .tgz files
+import  urllib.request
+#import  matplotlib.pyplot as plt
+
+# Fonction qui charge et décompresse les données
+
+def load_housing_data():
+    tarball_path = Path("datasets/housing.tgz")
+
+# Si les données ne sont pas déjà dans le dossier
+    if not tarball_path.is_file():
+
+# Crée le dossier `datasets`
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+
+# Va chercher les données à cette adresse
+        url = "http://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_path)
+
+# Le décompresse dans le dossier `datasets' do Working Directory
+        with tarfile.open(tarball_path) as housing_tarball:
+            housing_tarball.extractall(path="datasets")
+    return pd.read_csv(Path("datasets/housing/housing.csv"))
+
+housing = load_housing_data()
