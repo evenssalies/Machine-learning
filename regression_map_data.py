44d8d16048178a1b566ba961b91a8f64a1be3b7a
evenssalies <32770815+evenssalies@users.noreply.github.com>
2024-06-21 01:50:22 +0200

    Polished comments

--- regression_map_data.py
+++ regression_map_data.py
@@ -6,7 +6,6 @@ from    pathlib import Path
 import  pandas as pd
 import  tarfile         # Library to dezip .tgz files
 import  urllib.request
-#import  matplotlib.pyplot as plt
 
 # Fonction qui charge et décompresse les données
 
@@ -16,14 +15,14 @@ def load_housing_data():
 # Si les données ne sont pas déjà dans le dossier
     if not tarball_path.is_file():
 
-# Crée le dossier `datasets`
+# Crée le dossier `datasets'
         Path("datasets").mkdir(parents=True, exist_ok=True)
 
-# Va chercher les données à cette adresse
+# Va chercher le fichier de données à cette adresse
         url = "http://github.com/ageron/data/raw/main/housing.tgz"
         urllib.request.urlretrieve(url, tarball_path)
 
-# Le décompresse dans le dossier `datasets' do Working Directory
+# Le décompresse dans le dossier `datasets' du Working Directory
         with tarfile.open(tarball_path) as housing_tarball:
             housing_tarball.extractall(path="datasets")
     return pd.read_csv(Path("datasets/housing/housing.csv"))
